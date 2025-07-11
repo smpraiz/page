675d0d84cfa5c874d2cba7f848709530eeefdc82
smpraiz <smpraiz2025@gmail.com>
2025-07-11 20:16:45 +0000

    Discord Redirect

---
+++ pages/discord.jsx
@@ -0,0 +1,17 @@
+import { redirect } from "next/navigation"
+import Link from "next/link"
+
+export default function Discord() {
+
+    if(typeof window !== 'undefined') {
+        window.location.replace('https://discord.gg/n3uCfqdr77')
+    }
+
+
+    return (
+        <Link href={'https://discord.gg/n3uCfqdr77'}>
+            <h1>Redirecionando pro Discord...</h1>
+            <p>Se você não for redirecionado(a) em alguns instantes, clique aqui.</p>
+        </Link>
+    )
+}

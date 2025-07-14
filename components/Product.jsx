9ab27b80ae173206b2f629ed59c66a5c9443aac3
smpraiz <smpraiz2025@gmail.com>
2025-07-14 02:48:26 +0000

    Production version of site

--- components/Product.jsx
+++ components/Product.jsx
@@ -30,22 +30,6 @@ export const PRODUCTS = {
             description: 'Cansado(a) de ter que escolher entre sua casa, sua farm ou sua base secreta? Adicione ainda mais um ponto de /home no seu jogo e facilite sua vida no SMP!',
             expanded_description: 'Cansado(a) de ter que escolher entre sua casa, sua farm ou sua base secreta? Com esse produto, você ganha mais um slot de /home permanente! Assim, pode se teletransportar com facilidade para mais lugares importantes no seu mundo. Prático, rápido e perfeito pra quem joga sério.',
             icon: 'https://minecraft.wiki/images/Ender_Pearl_JE3_BE2.png?829a7',
-        },
-        {
-            name: 'Apoiador',
-            id: 'apoiador',
-            price: 5.00,
-            description: 'Mostre que você é um verdadeiro apoiador do SMP Raiz com o rank Apoiador! Com ele, você ganha um prefixo exclusivo, possibilidade de falar colorido, comando /chapéu e a satisfação de ajudar o servidor a crescer e melhorar cada vez mais.',
-            expanded_description: 'Mostre que você é um verdadeiro apoiador do SMP Raiz com o rank Apoiador! Com ele, você ganha um prefixo exclusivo, possibilidade de falar colorido, comando /chapéu e a satisfação de ajudar o servidor a crescer e melhorar cada vez mais. O rank Apoiador é vitalício, ou seja, você não precisa se preocupar em renovar ou perder suas vantagens. Além disso, você estará contribuindo para manter o servidor ativo, com novidades e eventos para toda a comunidade. Seja um Apoiador e faça parte dessa jornada!',
-            icon: 'https://minecraft.wiki/images/Nether_Star.gif?fb01f',
-        },
-        {
-            name: 'VIP (30 dias)',
-            id: 'vip1',
-            price: 7.99,
-            description: 'O VIP é o rank mais popular do servidor, com várias vantagens legais como redução no tempo de espera do /home e do /tpa, kits semanais (kit VIP), e 3 homes a mais de brinde!',
-            expanded_description: 'O VIP é o rank mais popular do servidor, com várias vantagens legais como redução no tempo de espera do /home e do /tpa, kits semanais (kit VIP), e 3 homes a mais de brinde! Além disso, você ajuda a manter o servidor ativo e em crescimento. O VIP dura 30 dias e é renovável.',
-            icon: 'https://minecraft.wiki/images/Nether_Star.gif?fb01f',
         }
     ]
 }
